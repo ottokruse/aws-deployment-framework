3fa5cb878e291fa796cb977018a60d8242ad0020
Otto Kruse <ottokrus@amazon.nl>
2019-05-17 17:06:58 +0200

    Fixes

--- src/initial/lambda_codebase/initial_commit/handler.py
+++ src/initial/lambda_codebase/initial_commit/handler.py
@@ -4,7 +4,7 @@ except Exception as e:  # pylint: disable=broad-except
     from urllib.request import Request, urlopen
     import json
 
-    def lambda_handler(event, _context):
+    def lambda_handler(event, _context, e=e):
         response = dict(
             LogicalResourceId=event["LogicalResourceId"],
             PhysicalResourceId=event.get("PhysicalResourceId", "NOT_YET_CREATED"),
